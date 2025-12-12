c45bfc58f9b64319351b614b04fc8994ef8c473f
Matthew Zhang <mtz99@users.noreply.github.com>
2025-12-11 22:08:14 -0800

    Comment update.

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -1,7 +1,7 @@
 /**
  * Removes the selected error from an errors object.
  * @param {Object} errors - The errors object
- * @param {string} errorKey - The key of the error to remove
+ * @param {string} errorToDelete - The key of the error to remove
  * @returns {Object} The empty errors object to return.
  */
 export const clearError = (errors, errorToDelete) => {
