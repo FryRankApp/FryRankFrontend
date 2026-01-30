cd9cb922020ee661a6749a87d38e1de6848eb88a
Alma Niu <almaniu@Almas-MacBook-Pro.local>
2026-01-29 21:46:37 -0800

    remove vibe coding donation feature

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import Reviews from './containers/Reviews';
 import Critic from './containers/Critic';
 import CreateReview from './containers/CreateReview';
 import RecentReviews from './components/RecentReviews';
-import Donate from './components/Donate';
 
 import {
     PATH_HOME,
@@ -13,8 +12,7 @@ import {
     PATH_ACCOUNT_REVIEWS,
     PATH_CREATE_REVIEW,
     PATH_RECENT_REVIEW,
-    PATH_USER_SETTINGS,
-    PATH_DONATE
+    PATH_USER_SETTINGS
 } from "./constants.js"
 import UserSettings from './containers/UserSettings'
 
@@ -26,7 +24,6 @@ const routes = [
     { path: PATH_RESTAURANTS, component: Restaurants },
     { path: PATH_RECENT_REVIEW, component: RecentReviews },
     { path: PATH_USER_SETTINGS, component: UserSettings},
-    { path: PATH_DONATE, component: Donate},
 ];
 
 const pathToPageName = {
